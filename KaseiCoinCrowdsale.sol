a9b2ec4d004afd153ec36597790ac856cea8eb50
malkawenedal <malkawenedal@gmail.com>
2022-04-26 14:02:23 -0600

    Provide parameters for all of the features of the crowdsale contract

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -12,7 +12,10 @@ contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANC
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
-        // YOUR CODE HERE!
+        uint256 rate,
+        address payable wallet,
+        KaseiCoin token
+      
     ) public Crowdsale(rate, wallet, token) {
         // constructor can stay empty
     }
